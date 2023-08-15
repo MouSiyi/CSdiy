f46a3b3174874cabf1a123398b66e9a82d0abddf
Bmanksy <bmanksy@163.com>
2023-08-15 19:54:40 +0800

    lab05 uploaded

---
+++ cs61a/Labs/lab05/lab05.py
@@ -0,0 +1,458 @@
+def flatten(s):
+    """Returns a flattened version of list s.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]     # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x # Ensure x is not mutated
+    [1, [2, 3], 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, 1]], 1, [1, 1]]
+    """
+    if len(s) == 0:
+        return s
+    elif len(s) == 1 and type(s[0]) != list: 
+        return s
+    else:
+        if type(s[0]) != list:
+          to_add = [s[0]]
+        else:
+          to_add = flatten(s[0])
+        return to_add + flatten(s[1:])
+# Base case设置几阶 看后续使用情况
+
+
+from math import sqrt
+
+
+def distance(city_a, city_b):
+    """
+    >>> city_a = make_city('city_a', 0, 1)
+    >>> city_b = make_city('city_b', 0, 2)
+    >>> distance(city_a, city_b)
+    1.0
+    >>> city_c = make_city('city_c', 6.5, 12)
+    >>> city_d = make_city('city_d', 2.5, 15)
+    >>> distance(city_c, city_d)
+    5.0
+    """
+    lat_a, lat_b = get_lat(city_a), get_lat(city_b)
+    lon_a, lon_b = get_lon(city_a), get_lon(city_b)
+    dis = sqrt((lat_a - lat_b)**2 + (lon_a - lon_b)**2)
+    return dis
+
+
+def closer_city(lat, lon, city_a, city_b):
+    """
+    Returns the name of either city_a or city_b, whichever is closest to
+    coordinate (lat, lon). If the two cities are the same distance away
+    from the coordinate, consider city_b to be the closer city.
+
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    """
+    des = make_city('Des', lat, lon)
+    distance_a, distance_b = distance(des, city_a), distance(des, city_b)
+    if distance_a < distance_b:
+        return get_name(city_a)
+    else:
+        return get_name(city_b)
+
+
+
+def check_city_abstraction():
+    """
+    There's nothing for you to do for this function, it's just here for the extra doctest
+    >>> change_abstraction(True)
+    >>> city_a = make_city('city_a', 0, 1)
+    >>> city_b = make_city('city_b', 0, 2)
+    >>> distance(city_a, city_b)
+    1.0
+    >>> city_c = make_city('city_c', 6.5, 12)
+    >>> city_d = make_city('city_d', 2.5, 15)
+    >>> distance(city_c, city_d)
+    5.0
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    >>> change_abstraction(False)
+    """
+
+# Treat all the following code as being behind an abstraction layer,
+# you shouldn't need to look at it.
+
+
+def make_city(name, lat, lon):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    >>> get_lat(city)
+    0
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return {"name": name, "lat": lat, "lon": lon}
+    else:
+        return [name, lat, lon]
+
+
+def get_name(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    """
+    if change_abstraction.changed:
+        return city["name"]
+    else:
+        return city[0]
+
+
+def get_lat(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lat(city)
+    0
+    """
+    if change_abstraction.changed:
+        return city["lat"]
+    else:
+        return city[1]
+
+
+def get_lon(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return city["lon"]
+    else:
+        return city[2]
+
+###############
+
+
+def berry_finder(t):
+    """Returns True if t contains a node with the value 'berry' and 
+    False otherwise.
+
+    >>> scrat = tree('berry')
+    >>> berry_finder(scrat)
+    True
+    >>> sproul = tree('roots', [tree('branch1', [tree('leaf'), tree('berry')]), tree('branch2')])
+    >>> berry_finder(sproul)
+    True
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> berry_finder(numbers)
+    False
+    >>> t = tree(1, [tree('berry',[tree('not berry')])])
+    >>> berry_finder(t)
+    True
+    """
+    if is_leaf(t):
+        if label(t) == 'berry':
+            return True
+        else:
+            return False
+    else:
+        find = False
+        for b in branches(t):
+            find = find or label(t) == 'berry' or berry_finder(b)
+        return find
+
+def sprout_leaves(t, leaves):
+    """Sprout new leaves containing the data in leaves at each leaf in
+    the original tree t and return the resulting tree.
+
+    >>> t1 = tree(1, [tree(2), tree(3)])
+    >>> print_tree(t1)
+    1
+      2
+      3
+    >>> new1 = sprout_leaves(t1, [4, 5])
+    >>> print_tree(new1)
+    1
+      2
+        4
+        5
+      3
+        4
+        5
+
+    >>> t2 = tree(1, [tree(2, [tree(3)])])
+    >>> print_tree(t2)
+    1
+      2
+        3
+    >>> new2 = sprout_leaves(t2, [6, 1, 2])
+    >>> print_tree(new2)
+    1
+      2
+        3
+          6
+          1
+          2
+    """
+    if is_leaf(t):
+        return tree(label(t), [tree(b) for b in leaves])
+    else:
+        return tree(label(t), [sprout_leaves(b, leaves) for b in branches(t)])
+  
+
+# Abstraction tests for sprout_leaves and berry_finder
+
+
+def check_abstraction():
+    """
+    There's nothing for you to do for this function, it's just here for the extra doctest
+    >>> change_abstraction(True)
+    >>> scrat = tree('berry')
+    >>> berry_finder(scrat)
+    True
+    >>> sproul = tree('roots', [tree('branch1', [tree('leaf'), tree('berry')]), tree('branch2')])
+    >>> berry_finder(sproul)
+    True
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> berry_finder(numbers)
+    False
+    >>> t = tree(1, [tree('berry',[tree('not berry')])])
+    >>> berry_finder(t)
+    True
+    >>> t1 = tree(1, [tree(2), tree(3)])
+    >>> print_tree(t1)
+    1
+      2
+      3
+    >>> new1 = sprout_leaves(t1, [4, 5])
+    >>> print_tree(new1)
+    1
+      2
+        4
+        5
+      3
+        4
+        5
+
+    >>> t2 = tree(1, [tree(2, [tree(3)])])
+    >>> print_tree(t2)
+    1
+      2
+        3
+    >>> new2 = sprout_leaves(t2, [6, 1, 2])
+    >>> print_tree(new2)
+    1
+      2
+        3
+          6
+          1
+          2
+    >>> change_abstraction(False)
+    """
+
+
+def preorder(t):
+    """Return a list of the entries in this tree in the order that they
+    would be visited by a preorder traversal (see problem description).
+
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> preorder(numbers)
+    [1, 2, 3, 4, 5, 6, 7]
+    >>> preorder(tree(2, [tree(4, [tree(6)])]))
+    [2, 4, 6]
+    """
+    if is_leaf(t):
+        return [label(t)]
+    else:
+        return [label(t)] + sum([preorder(b) for b in branches(t)], [])
+
+from itertools import zip_longest
+
+def add_trees(t1, t2):
+    """
+    >>> numbers = tree(1,
+    ...                [tree(2,
+    ...                      [tree(3),
+    ...                       tree(4)]),
+    ...                 tree(5,
+    ...                      [tree(6,
+    ...                            [tree(7)]),
+    ...                       tree(8)])])
+    >>> print_tree(add_trees(numbers, numbers))
+    2
+      4
+        6
+        8
+      10
+        12
+          14
+        16
+    >>> print_tree(add_trees(tree(2), tree(3, [tree(4), tree(5)])))
+    5
+      4
+      5
+    >>> print_tree(add_trees(tree(2, [tree(3)]), tree(2, [tree(3), tree(4)])))
+    4
+      6
+      4
+    >>> print_tree(add_trees(tree(2, [tree(3, [tree(4), tree(5)])]), \
+    tree(2, [tree(3, [tree(4)]), tree(5)])))
+    4
+      6
+        8
+        5
+      5
+    """
+    if is_tree(t1) and t2 == None:
+        return t1
+    elif t1 == None and is_tree(t2):
+        return t2
+    else:
+        return tree(label(t1)+label(t2), [add_trees(b[0], b[1]) for b in zip_longest(branches(t1), branches(t2))])
+
+
+
+    #考虑深度
+    '''
+    if is_leaf(t1) and is_leaf(t2):
+      return tree(label(t1)+label(t2))
+    elif is_leaf(t1) and not is_leaf(t2):
+      return tree(label(t1)+label(t2), [add_trees(tree(0), b) for b in branches(t2)])
+    elif is_leaf(t2) and not is_leaf(t1):
+      return tree(label(t1)+label(t2), [add_trees(tree(0), b) for b in branches(t1)])
+    else:
+        #考虑长度
+        diff = len(branches(t1)) - len(branches(t2))
+        if diff > 0:
+          branches(t2).extend([tree(0)] * diff)
+        elif diff < 0:
+          branches(t1).extend([tree(0)] * (-diff))
+        return tree(label(t1)+label(t2), [add_trees(b[0], b[1]) for b in zip(branches(t1), branches(t2))])
+    '''
+          
+    
+            
+    
+
+
+def change_abstraction(change):
+    """
+    For testing purposes.
+    >>> change_abstraction(True)
+    >>> change_abstraction.changed
+    True
+    """
+    change_abstraction.changed = change
+
+
+change_abstraction.changed = False
+
+
+# Tree ADT
+
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    if change_abstraction.changed:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return {'label': label, 'branches': list(branches)}
+    else:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return [label] + list(branches)
+
+
+def label(tree):
+    """Return the label value of a tree."""
+    if change_abstraction.changed:
+        return tree['label']
+    else:
+        return tree[0]
+
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    if change_abstraction.changed:
+        return tree['branches']
+    else:
+        return tree[1:]
+
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if change_abstraction.changed:
+        if type(tree) != dict or len(tree) != 2:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+    else:
+        if type(tree) != list or len(tree) < 1:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
+    """
+    return not branches(tree)
+
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the root.
+
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
